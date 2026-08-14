b4320806c3d44f6fc05c8dbf54d62543157c1347
Zeronetsec <senxhunter996@gmail.com>
2026-08-14 23:58:00 +0700

    modified: .install/extern/privdat.sh

--- .install/extern/privdat.sh
+++ .install/extern/privdat.sh
@@ -1,9 +1,16 @@
 function install::extern::privdat() {
     if [[ -d "${root}/.privdat" ]]; then
         echo -e "${B}[*] ${N}Setting up private data..."
-        command rm -rf "${root}/data/user_data"
-        command cp -r \
-            "${root}/.privdat" \
-            "${root}/data/user_data"
+        install::getinstall \
+            "command rm -rf ${root}/data/user_data" \
+            "Removing: ${GG}${root}/data/user_data${N}"
+
+        install::getinstall \
+            "
+                command cp -r \
+                    ${root}/.privdat \
+                    ${root}/data/user_data
+            " \
+            "Copying: ${GG}${root}/.privdat ${DG}-> ${GG}${root}/data/user_data${N}"
     fi
 }; readonly -f install::extern::privdat
